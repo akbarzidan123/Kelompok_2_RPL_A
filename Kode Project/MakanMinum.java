b6454ae97f334db6bbf7e17cdd925ad51713d8ed
akbarzidan123 <63868719+akbarzidan123@users.noreply.github.com>
2020-12-06 15:48:00 +0700

    Update MakanMinum.java

--- Kode Project/MakanMinum.java
+++ Kode Project/MakanMinum.java
@@ -7,22 +7,26 @@ public class MakanMinum{
 	public int [] hargaMinum = {40_000,30_000,45_000,48_000,55_000};
  
  	//Makanan yang dibeli
- 	public ArrayList <String> makananUser = new ArrayList <String>();
- 	public ArrayList <Integer> jumMakananUser = new ArrayList <Integer>();
- 	public int totalMakanan = 0;
+ 	public static ArrayList <String> makananUser = new ArrayList <String>();
+ 	public static ArrayList <Integer> jumMakananUser = new ArrayList <Integer>();
+ 	public static int totalMakanan = 0;
 
  	//Minuman yang dibeli
- 	public ArrayList <String> minumanUser = new ArrayList <String>();
- 	public ArrayList <Integer> jumMinumanUser = new ArrayList <Integer>();
- 	public int totalMinuman = 0;
+ 	public static ArrayList <String> minumanUser = new ArrayList <String>();
+ 	public static ArrayList <Integer> jumMinumanUser = new ArrayList <Integer>();
+ 	public static int totalMinuman = 0;
 
  	//Konstruktor MakanMinum
- 	public MakanMinum(Scanner huruf,Scanner angka){
- 		makananMinuman(huruf,angka);
+ 	public MakanMinum(){
+
+ 	}
+
+ 	public MakanMinum(Scanner in){
+ 		makananMinuman(in);
  	}
 
  	//Menampilkan pilihan makanan atau minuman
-	public void makananMinuman(Scanner huruf,Scanner angka){
+	public void makananMinuman(Scanner in){
 		System.out.println("===============================");
 		System.out.println("     Makanan dan Minuman");
 		System.out.println("===============================");
@@ -31,47 +35,45 @@ public class MakanMinum{
 		System.out.println("3. Lanjut Isi Data Diri");
 		System.out.println("4. Batalkan Pemesanan");
 		System.out.print("Pilih: ");
-		int pilih = angka.nextInt();
+		int pilih = in.nextInt();
 		switch(pilih){
 			case 1:
-			makanan(huruf,angka);
+			makanan(in);
 			break;
 			case 2:
-			minuman(huruf,angka);
+			minuman(in);
 			break;
 			case 3:
-			InputDataDiri input = new InputDataDiri(huruf,angka);
+			InputDataDiri input = new InputDataDiri(in);
 			break;
 			case 4:
 			System.exit(1);
 			default:
-			makananMinuman(huruf,angka);
+			makananMinuman(in);
 			break;
 		}
 	}
 
-	public void minuman(Scanner huruf,Scanner angka){
+	public void minuman(Scanner in){
 		boolean loop = true;
 		do{
-			System.out.println("\n");
+			System.out.println("=================================");
 			System.out.println(" Daftar Minuman");
 			System.out.println("=================================");
-			System.out.println(String.format("1. %-15s Rp.40.000","Iced Lychee"));
-			System.out.println(String.format("2. %-15s Rp.30.000","Iced Peach"));
-			System.out.println(String.format("3. %-15s Rp.45.000","Iced Mochaccino"));
-			System.out.println(String.format("4. %-15s Rp.48.000","Iced	Mojito Lime"));
-			System.out.println(String.format("5. %-15s Rp.55.000","Iced Blenccino"));
+			for (int i = 1; i < minuman.length ; i++ ) {
+				System.out.println(String.format(i+". %-20s "+hargaMinum[i-1], minuman[i-1]));	
+			}
 			System.out.print("Pilih: " );
-			int pilihMinuman = angka.nextInt();
+			int pilihMinuman = in.nextInt();
 			System.out.print("\nJumlah Minuman Yang ingin dibeli: ");
-			int jumlahMinuman= angka.nextInt();
+			int jumlahMinuman= in.nextInt();
 
 			minumanUser.add(minuman[pilihMinuman-1]);
 			totalMinuman += hargaMinum[pilihMinuman-1]*jumlahMinuman;
 			jumMinumanUser.add(jumlahMinuman);
 
 			System.out.print("\nApakah ingin membeli Minuman lagi?(Y/N) ");
-			String check = huruf.next();
+			String check = in.next();
 			if(check.equalsIgnoreCase("Y")){
 				loop = true;
 			}
@@ -82,34 +84,32 @@ public class MakanMinum{
 					System.out.println((i+1)+". "+ minumanUser.get(i) +" sebanyak: " + jumMinumanUser.get(i));
 				}
 
-				makananMinuman(huruf,angka);
+				makananMinuman(in);
 			}	
 		}while(loop);
 
 	}
 
-	public void makanan(Scanner huruf,Scanner angka){
+	public void makanan(Scanner in){
 		boolean loop = true;
 		do{
-			System.out.println("\n");
+			System.out.println("=================================");
 			System.out.println(" Daftar Makanan");
 			System.out.println("=================================");
-			System.out.println(String.format("1. %-20s Rp.35.000","French Fries"));
-			System.out.println(String.format("2. %-20s Rp.30.000","Pisang Goreng"));
-			System.out.println(String.format("3. %-20s Rp.20.000","Popcorn Salt"));
-			System.out.println(String.format("4. %-20s Rp.35.000","Chicken Burger"));
-			System.out.println(String.format("5. %-20s Rp.23.000","Popcorn Sweet Glaze"));
+			for(int i = 1 ; i < makanan.length; i++){
+				System.out.println(String.format(i+". %-20s "+hargaMakan[i-1], makanan[i-1]));
+			}
 			System.out.print("Pilih: " );
-			int pilihMakanan = angka.nextInt();
+			int pilihMakanan = in.nextInt();
 			System.out.print("\nJumlah Makanan Yang ingin dibeli: ");
-			int jumlahMakanan= angka.nextInt();
+			int jumlahMakanan= in.nextInt();
 
 			makananUser.add(makanan[pilihMakanan-1]);
 			totalMakanan += hargaMakan[pilihMakanan-1]*jumlahMakanan;
 			jumMakananUser.add(jumlahMakanan);
 			
 			System.out.print("\nApakah ingin membeli Makanan lagi?(Y/N) ");
-			String check = huruf.next();
+			String check = in.next();
 			if(check.equalsIgnoreCase("Y")){
 				loop = true;
 			}
@@ -120,7 +120,7 @@ public class MakanMinum{
 					System.out.println((i+1)+". "+ makananUser.get(i) +" sebanyak: " + jumMakananUser.get(i));
 				}
 
-				makananMinuman(huruf,angka);
+				makananMinuman(in);
 			}	
 		}while(loop);
 	}
