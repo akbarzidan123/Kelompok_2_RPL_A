97910daddbb60615a02ffb2be08531f15401c3b1
akbarzidan123 <63868719+akbarzidan123@users.noreply.github.com>
2020-12-06 15:44:01 +0700

    Update ProjectRPL.java

--- Kode Project/ProjectRPL.java
+++ Kode Project/ProjectRPL.java
@@ -1,12 +1,17 @@
 import java.util.*;
+/**
+* @author: Akbar Zidan Al Zakki (1402018067)
+*			
+*
+*/
 public class ProjectRPL{
 	public static void main(String[] args) {
-		Scanner huruf = new Scanner(System.in);
-		Scanner angka = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		System.out.println("-----------------------------------------");
-		System.out.println("      Selamat Datang Di....         ");
+		System.out.println("      Selamat Datang Di BiosScope       ");
 		System.out.println("-----------------------------------------");
-		// InputDataDiri inp = new InputDataDiri(in);
-		FilmBioskop film = new FilmBioskop(huruf,angka);
+		
+		FilmBioskop film = new FilmBioskop(in);
+		
 	}
 }
